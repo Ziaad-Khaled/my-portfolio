ab8afcdef2ae9487537e92cb32121a79e4fa04da
Ziaad-Khaled <ziadkhaled1918@gmail.com>
2023-10-02 11:19:47 +0300

    Changed job description

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Computer Engineer",
+          "VR Developer",
+          "Game Developer",
+          "Data Engineer",
         ],
         autoStart: true,
         loop: true,
