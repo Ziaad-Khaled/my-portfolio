3e77fadce7114123cc53500629c0100a50b10bbb
Ziaad-Khaled <ziadkhaled1918@gmail.com>
2023-12-12 14:52:10 +0200

    Back after the bootcamp

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,37 +19,22 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+              Passionate Computer Engineer with a Bachelor’s from the German University in Cairo. 
+              Skilled in crafting immersive experiences and data-driven solutions using 
+              <b className="purple"> Java </b>, 
+              <b className="purple"> C# </b>, 
+              and 
+              <b className="purple"> Python</b>.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              At Fraunhofer Heinrich-Hertz Institute, I delved into computer vision and machine learning, 
+              focusing on dynamic gesture recognition. Using Microsoft .Net (C#), Python, and OpenCV, 
+              I translated human movements into interactive systems.
+              <br />
+              <br />
+              Beyond gaming, my interest in data science led me to explore statistical learning models, 
+              regression, and classification algorithms during a machine learning internship. 
+              Python, Jupyter Notebook, and Scikit-learn were my tools in this data-driven journey.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
